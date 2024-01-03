d0620d9306824ce46af1c93855305c79354b75be
n00b74cu14r <59424730+n00b74cu14r@users.noreply.github.com>
2024-01-02 19:47:13 -0500

    inital
    Added the installer file, and readme.

---
+++ AIMS-3.0-Javascript-Installer.ps1
@@ -0,0 +1,101 @@
+#-----------------------------------------------------------------#
+# Created 01/02/2024 for use with AIMS 3.0 Database installation. #
+#-----------------------------------------------------------------#
+
+# This is the javascript code that will be written to the file.
+$jsCode = 
+@"
+        function LockAfterSigning(sMyActions, sMyFields, sMySignature) { 
+            app.beginPriv();
+
+                var arrMyFields = sMyFields.split(','); 
+                var MyLock = {action:sMyActions,fields:arrMyFields}; 
+                var f = this.getField(sMySignature); 
+                var oLock = f.getLock(); 
+                oLock = MyLock; 
+                f.setLock(oLock); 
+            app.endPriv(); 
+        }
+"@
+
+Write-Host "            AIMS 3.0 Javacript Installer"
+Write-Host "-----------------------------------------------------"
+Write-Host "Please select your user type:"
+Write-Host "Admin User - [1] - *Administator privileges required*"
+Write-Host "Basic User - [2]"
+Write-Host "Exit       - [0]"
+Write-Host " "
+Write-Host "-----------------------------------------------------"
+$choice = Read-Host "Enter your choice: "
+
+switch ($choice) {
+    1 { # Workstation Admin
+        # Code Snippet from https://stackoverflow.com/questions/63342982/how-to-powershell-script-ask-for-administrator-rights
+        # Forces UAC pop up for admin rights. User will have to go through the selection and select 1 again to complete the script.
+        # Can be bypassed if they run from powershell as an admin initially.
+        if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
+            Exit
+        # End Snippet
+        } else 
+            {
+            # This portion will run if admin rights are present.
+            Write-Host "You selected admin user, checking for directory now..."
+            Write-Host "Checking for directory now..."
+            $jsDirectory = "C:\Program Files (x86)\Adobe\Acrobat X.0\Acrobat\Javascripts\"
+            
+            # Check if directory exists, if not create it.
+            if (-not (Test-Path $jsDirectory)) {
+                Write-Host "Directory not found, creating..."
+                New-Item -ItemType Directory -Path $jsDirectory | Out-Null
+                
+            } else {
+                # Directory exists, continue.
+                Write-Host "Directory found, continuing..."
+            }     
+
+            # Create the LockAfterSigning.js file in the created directory.
+            Write-Host "Creating LockAfterSigning.js..."
+            $jsFilePath = Join-Path -Path $jsDirectory -ChildPath "LockAfterSigning.js"
+            $jsCode | Out-File -FilePath $jsFilePath -Encoding UTF8
+            Write-Host "Complete!"
+            Write-Host "-----------------------------------------------------"
+            
+            #Press any key to continue...
+            cmd /c pause
+        }
+    }
+    2 { # Basic User
+        Write-Host "You selected basic user, checking for directory now..."
+        Write-Host "Checking for directory now..."
+        $currentUser = $env:USERNAME
+        $jsDirectory = "C:\Users\$currentUser\AppData\Roaming\Adobe\Acrobat\Privileged\DC\JavaScripts"
+        
+        # Check if directory exists, if not create it.
+        if (-not (Test-Path $jsDirectory)) {
+            Write-Host "Directory not found, creating..."
+            New-Item -ItemType Directory -Path $jsDirectory | Out-Null
+            
+        } else {
+            # Directory exists, continue.
+            Write-Host "Directory found, continuing..."
+        }     
+
+        # Create the LockAfterSigning.js file in the created directory.
+        Write-Host "Creating LockAfterSigning.js..."
+        $jsFilePath = Join-Path -Path $jsDirectory -ChildPath "LockAfterSigning.js"
+        $jsCode | Out-File -FilePath $jsFilePath -Encoding UTF8
+        Write-Host "Complete!"
+        Write-Host "-----------------------------------------------------"
+
+        #Press any key to continue...
+        cmd /c pause
+    }
+    0 { # Exit
+        Write-Host "Exiting..."
+        return
+    }
+    default { # Invalid choice
+        Write-Host "Invalid choice. Please enter 1, 2, or 0."
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # AIMS-3.0-Javascript-Installer
-Helps with the installation of AIMS 3.0 Database.
+
+This repository contains the AIMS 3.0 Javascript Installer, which helps with the installation of the AIMS 3.0 Database.
+
+## Required
+This script requires the use of:
+
+1. [PowerShell](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.4#winget) (should be pre-installed on windows computers)
+
+## Dependencies (After Installation)
+1. Microsoft Access
+2. [AIMS 3.0 Database](https://www.tactical-tech.net/database.php) - From Tactical-Tech.
+3. Adobe Acrobat Pro
+
+## Installation
+
+To install the AIMS Javascript file, follow these steps:
+
+1. Save AIMS-3.0-Javascript-Installer.ps1 to your computer. 
+2. Right click and run with powershell.
+3. If on a government computer you may be prompted to hit [Y] to acknowledge you want to run an unknown script.
+4. You will be prompted to enter a selection.
+```
+            AIMS 3.0 Javacript Installer
+-----------------------------------------------------
+Please select your user type:
+Admin User - [1] - *Administator privileges required*
+Basic User - [2]
+Exit       - [0]
+
+-----------------------------------------------------
+```
+Select 1 if you are an administrator or 2 if you are a basic user (no admin rights)
+
+4. Once the installation is complete, you can start using the AIMS 3.0 Database.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
